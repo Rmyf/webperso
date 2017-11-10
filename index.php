f2ff369d4f825d7ad85e1bb1538a3f0f6fd8086f
rmyf <remy.furnari@gmail.com>
2017-11-10 10:04:09 +0100

    bootstrapversion

--- index.php
+++ index.php
@@ -30,7 +30,7 @@ foreach($all as $f) {
     <!-- header-navabar -->
     <header class="container-fluid red">
         <div class="container-fluid row boxheader">
-            <div class="col-md-1  green">
+            <div class="col-md-1  logo">
                 <a href="https://www.simplonlyon.fr/promo3/rfurnari/"><img src="./images/monogram5.png" alt="monogram RF" class="monogram"></a>
             </div>
 
@@ -50,7 +50,7 @@ foreach($all as $f) {
                                 </ul> -->
                             </a>
                         </li>
-                        <li><a href="#01">Contact</a></li>
+                        <li><a href="#01" class="offset-md-2">Contact</a></li>
                     </ul>
                 </nav>
             </div>
@@ -58,83 +58,73 @@ foreach($all as $f) {
     </header>
 
     <!-- projet -->
-    <section class="container-fluid green">
-        <div class="boxtitle">
-            <div class="nom">
+    <section class="container-fluid main">
+        <div class="col-md-10 row boxtitle">
+            <div class="offset-md-5 nom">
                 <h3>Rémy FURNARI</h3>
             </div>
             
-            <div class="titre">
+            <div class="offset-md-1 titre">
                 <h1>Développeur web</h1>
             </div>
         </div>
     
-        <div class="box">
-            <div class="project">
+        <div class="row box">
+            <div class="offset-md-1 col-md-4 row project">
                 <a href="#mesprojets" class="button">Découvrez mes projets</a>
-                <img class="screen"src="images/mario.png" width="130" height="90">
-                <img class="screen"src="images/HelpInCode.png" width="130" height="90">
+                    <div class="offset-md-1 col-md-5 screentop">
+                        <img class="img-responsive screen"src="images/mario.png" width="130" height="90">
+                        <img class="img-responsive screen"src="images/HelpInCode.png" width="130" height="90">
+                    </div>
+                    <div class="col-md-5 screenbottom">
+                        <img class="img-responsive screen"src="images/elievignal.png" width="130" height="90">
+                        <img class="img-responsive screen"src="images/HelpInCode.png" width="130" height="90">
+                    </div>
             </div>
             
-            <div class="cv">
-                <img class="screen"src="images/cv_frame.png" width="260" height="180">
-                <a href="./cv.html" class="button">Mon CV</a> 
+            <div class="offset-md-2 col-md-4 cv">
+                <img class="offset-md-3 img-responsive cvimg"src="images/cv_frame.png" width="260" height="180">
+                <a href="./cv.html" class="offset-md-4 button">Mon CV</a> 
             </div> 
         </div>
     </section>
+
+
+    <div class="list">
+        <div class="row toplist">
+            <section class="offset-md-1">
+                <a href="http://176.142.133.249/projet-wordpress-simplon/wordpress/">
+                <img src="images/elievignal.png" class="img-responsive screenimg" width="600" height="300" alt="help"></a>
+                <h2 class="">Cité Scolaire Elie Vignal</h2>
+                <p class=""></p>
+                <a class="" href="">GitHub</a>
+            </section>
+        
+                
+             <section class="offset-md-1">
+                 <img src="images/HelpInCode.png" class="img-responsive screenimg" width="600" height="300" alt="help">
+                 <h2 class="">Forum</h2>
+                 <p class=""></p>
+                 <a class="" href="">GitHub</a>
+            </section>
+    </div>
+        <div class="row bottomlist">    
+            <section class="offset-md-1">
+                 <img src="images/mario.png" class="img-responsive screenimg" width="600" height="300" alt="game">
+                 <h2 class="">Game</h2>
+                 <p class="">Jeu réalisé en Javascript</p>
+                 <a class="" href="">GitHub</a>
+            </section>
+        </div>
          
-            <!-- <section class="main">
-                <div class="boxtitle">
-                    <div class="nom">
-                        <h3>Rémy FURNARI</h3>
-                    </div>
-
-                    <div class="titre">
-                        <h1>Développeur web</h1>
-                    </div>
-                </div>
-                
-                <div class="box">
-                    <div class="project">
-                        <a href="#mesprojets" class="button">Découvrez mes projets</a>
-                        <img class="screen"src="images/mario.png" width="130" height="90">
-                        <img class="screen"src="images/HelpInCode.png" width="130" height="90">
-                    </div>
-
-                    <div class="cv">
-                        <img class="screen"src="images/cv_frame.png" width="260" height="180">
-                        <a href="./cv.html" class="button">Mon CV</a> 
-                    </div>
-
-                </div>
-            </section> -->
-                
-            <!-- <div clas="list">
-                <section class="">
-                    <img src="images/mario.png" class="" width="500" height="300" alt="game">
-                    <h2 class="">Game</h2>
-                    <p class=""></p>
-                    <a class="" href="">lien</a>
-                    <a class="" href="">GitHub</a>
-                </section>
-                
-                <section class="">
-                    <img src="images/HelpInCode.png" class="" width="500" height="300" alt="help">
-                    <h2 class="">Forum</h2>
-                    <p class=""></p>
-                    <a class="" href="">lien</a>
-                        <a class="" href="">GitHub</a>
-                </section>
-            </div> -->
-       
-
-        <!-- <footer>
+           
+        <footer>
             <ul class="socialLink" id="01">
                 <li><a href="https://github.com/Rmyf" class="fa fa-github fa-3x" aria-hidden="true"></a></li>
                 <li><a href="https://www.linkedin.com/in/rémy-furnari" class="fa fa-linkedin-square fa-3x" aria-hidden="true"></a></li>
                 <li><a href="https://twitter.com/Remy_FURNARI" class="fa fa-twitter fa-3x" aria-hidden="true"></a></li>
                 <li><a href="mailto:remy.furnari@gmail.com" class="fa fa-envelope fa-3x" aria-hidden="true"></a></li>
             </ul>
-        </footer>         -->
+        </footer>        
     </body>
 </html>
